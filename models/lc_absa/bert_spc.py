725dcf59e620e2f9c69e8bb7e68e9bf2b7e77034
yangheng <yangheng95@163.com>
2020-06-29 13:39:28 +0800

    first commit

---
+++ models/lc_absa/bert_spc.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# file: BERT_SPC.py
+# author: songyouwei <youwei0314@gmail.com>
+# Copyright (C) 2019. All Rights Reserved.
+
+import torch.nn as nn
+
+
+class BERT_SPC(nn.Module):
+    def __init__(self, bert, opt):
+        super(BERT_SPC, self).__init__()
+        self.bert = bert
+        self.opt = opt
+        self.dropout = nn.Dropout(opt.dropout)
+        self.dense = nn.Linear(opt.embed_dim, opt.polarities_dim)
+
+    def forward(self, inputs):
+        text_bert_indices = inputs[0]
+        bert_segments_ids = inputs[1]
+        _, pooled_output = self.bert(text_bert_indices, token_type_ids=bert_segments_ids)
+        pooled_output = self.dropout(pooled_output)
+        logits = self.dense(pooled_output)
+        return logits
---
+++ models/lc_absa/hlcf_bert.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# file: hlcf_bert.py
+# author: yangheng <yangheng@m.scnu.edu.cn>
+# Copyright (C) 2020. All Rights Reserved.
+
+import torch
+import torch.nn as nn
+import numpy as np
+import copy
+
+from pytorch_transformers.modeling_bert import BertPooler, BertSelfAttention
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, config, opt):
+        super(SelfAttention, self).__init__()
+        self.opt = opt
+        self.config = config
+        self.SA = BertSelfAttention(config)
+        self.tanh = torch.nn.Tanh()
+
+    def forward(self, inputs):
+        zero_vec = np.zeros((inputs.size(0), 1, 1, self.opt.max_seq_len))
+        zero_tensor = torch.tensor(zero_vec).float().to(self.opt.device)
+        SA_out = self.SA(inputs, zero_tensor)
+        return self.tanh(SA_out[0])
+
+
+class HLCF_BERT(nn.Module):
+    def __init__(self, bert, opt):
+        super(HLCF_BERT, self).__init__()
+        pass
+
+    def forward(self, inputs):
+        pass
