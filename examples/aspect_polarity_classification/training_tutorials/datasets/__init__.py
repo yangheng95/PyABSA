f3a5fcf8eb5e55c5637c1c8a2d19f61d3db01194
yangheng <yangheng95@163.com>
2021-06-09 09:23:59 +0800

    0.6.7.3

--- examples/aspect_polarity_classification/training_tutorials/datasets/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# file: __init__.py
-# time: 2021/6/8 0008
-# author: yangheng <yangheng@m.scnu.edu.cn>
-# github: https://github.com/yangheng95
-# Copyright (C) 2021. All Rights Reserved.
