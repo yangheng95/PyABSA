0f72527dd9fc9f8397044bac4c60603b00cfb26d
yangheng <yangheng95@163.com>
2021-06-09 12:22:12 +0800

    0.6.8.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyabsa',
-    version='0.6.8.1',
+    version='0.6.8.2',
     description='This tool provides the sota models for aspect term extraction (ATE) '
                 'and aspect polarity classification (APC)',
     # The project's main homepage.
