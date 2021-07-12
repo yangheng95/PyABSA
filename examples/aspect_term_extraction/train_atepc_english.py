2502090b9d0c43b3a28d5d8ec2e9178ae3076e5a
Yang Heng <yangheng95@163.com>
2021-07-12 16:06:54 +0800

    0.8.8.4

--- examples/aspect_term_extraction/train_atepc_english.py
+++ examples/aspect_term_extraction/train_atepc_english.py
@@ -24,21 +24,8 @@ param_dict['evaluate_begin'] = 5
 param_dict['num_epoch'] = 6
 param_dict['log_step'] = 100
 semeval = ABSADatasets.SemEval
-# aspect_extractor = train_atepc(parameter_dict=param_dict,      # set param_dict=None to use default model
-#                                dataset_path=semeval,           # file or dir, dataset(s) will be automatically detected
-#                                model_path_to_save=save_path,   # set model_path_to_save=None to avoid save model
-#                                auto_evaluate=True,             # evaluate model while training_tutorials if test set is available
-#                                auto_device=True                # Auto choose CUDA or CPU
-#                                )
-
-param_dict = atepc_config_handler.get_atepc_param_dict_multilingual()
-param_dict['evaluate_begin'] = 4
-param_dict['num_epoch'] = 5
-param_dict['learning_rate'] = 3e-5
-save_path = 'state_dict'
-multilingual = ABSADatasets.Multilingual
 aspect_extractor = train_atepc(parameter_dict=param_dict,      # set param_dict=None to use default model
-                               dataset_path=multilingual,    # file or dir, dataset(s) will be automatically detected
+                               dataset_path=semeval,           # file or dir, dataset(s) will be automatically detected
                                model_path_to_save=save_path,   # set model_path_to_save=None to avoid save model
                                auto_evaluate=True,             # evaluate model while training_tutorials if test set is available
                                auto_device=True                # Auto choose CUDA or CPU
