ed86903aa66b2e8a3478a369544f5f6bca6590b3
Yang Heng <yangheng95@163.com>
2021-09-28 15:10:25 +0800

    1.1.22

--- examples/aspect_polarity_classification/sentiment_inference_chinese.py
+++ examples/aspect_polarity_classification/sentiment_inference_chinese.py
@@ -9,15 +9,15 @@
 from pyabsa import APCCheckpointManager, ABSADatasetList
 
 # Assume the sent_classifier is loaded or obtained using train function
-sent_classifier = APCCheckpointManager.get_sentiment_classifier(checkpoint='fast_lcfs_bert_acc_95.71_f1_93.61')
+sent_classifier = APCCheckpointManager.get_sentiment_classifier(checkpoint='fast_lcfs_bert_acc_91.37_f1_63.13')
 
 # # 由于BERT采用单字分词，中文是否用空格分割不影响BERT的表现。欢迎贡献中文或其它语言数据集
 # chinese_text = '还有就是[ASP]笔画的键盘分布[ASP]我感觉不合理!sent! 0'
 # sent_classifier.infer(chinese_text, print_result=True)
 
 # infer_set = ABSADatasetList.Chinese
-# infer_set = ABSADatasetList.Shampoo
-infer_set = 'mooc'
+infer_set = ABSADatasetList.Shampoo
+# infer_set = 'mooc'
 
 results = sent_classifier.batch_infer(target_file=infer_set,
                                       print_result=True,
