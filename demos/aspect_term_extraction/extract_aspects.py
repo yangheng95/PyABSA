26bc014f25c0bef261766313646b203ec10de93a
Yang Heng <yangheng95@163.com>
2021-12-16 11:58:12 +0000

    1.6.11

--- demos/aspect_term_extraction/extract_aspects.py
+++ demos/aspect_term_extraction/extract_aspects.py
@@ -7,26 +7,20 @@
 from pyabsa import ABSADatasetList, available_checkpoints
 from pyabsa import ATEPCCheckpointManager
 
-checkpoint_map = available_checkpoints(from_local=False)
+# checkpoint_map = available_checkpoints(from_local=False)
 
-examples = ['But the staff was so nice to us .',
-            'But the staff was so horrible to us .',
-            r'Not only was the food outstanding , but the little ` perks \' were great .',
-            'It took half an hour to get our check , which was perfect since we could sit , have drinks and talk !',
-            'It was pleasantly uncrowded , the service was delightful , the garden adorable , '
-            'the food -LRB- from appetizers to entrees -RRB- was delectable .',
-            'How pretentious and inappropriate for MJ Grill to claim that it provides power lunch and dinners !'
+examples = ['But the staff was so perfect to us, but the service was bad .',
             ]
 
 aspect_extractor = ATEPCCheckpointManager.get_aspect_extractor(checkpoint='english',
-                                                               auto_device=False  # False means load model on CPU
+                                                               auto_device=True  # False means load model on CPU
                                                                )
 
-inference_source = ABSADatasetList.SemEval
+# inference_source = ABSADatasetList.SemEval
 # inference_source = r'E:\PyABSA-Workspace\latest\PyABSA\examples\aspect_polarity_classification\integrated_datasets\datasets\apc_datasets\TShirt'
 # inference_source = 'integrated_datasets/datasets/apc_datasets/TShirt'
 # inference_source = ABSADatasetList.TShirt
-# inference_source = examples
+inference_source = examples
 # inference_source = ABSADatasetList.Laptop14
 atepc_result = aspect_extractor.extract_aspect(inference_source=inference_source,  #
                                                save_result=True,
