9c851e69b3aba22f4783867263da3a207be4b6f0
Yang Heng <yangheng95@163.com>
2021-12-25 21:09:51 +0000

    1.6.14

--- demos/aspect_polarity_classification/sentiment_inference.py
+++ demos/aspect_polarity_classification/sentiment_inference.py
@@ -13,33 +13,33 @@ os.environ['PYTHONIOENCODING'] = 'UTF8'
 
 checkpoint_map = available_checkpoints(from_local=False)
 
-examples = [
-    'The [ASP]battery-life[ASP], and this [ASP]battery[ASP] is ok',
-    'The [ASP] battery-life [ASP] is bad',
-    'The [ASP] battery-life [ASP] is good',
-    'The [ASP] battery-life [ASP] ',
-    'Strong build though which really adds to its [ASP]durability[ASP] .',  # !sent! Positive
-    'Strong [ASP]build[ASP] though which really adds to its durability . !sent! Positive',
-    'The [ASP]battery life[ASP] is excellent - 6-7 hours without charging . !sent! Positive',
-    'I have had my computer for 2 weeks already and it [ASP]works[ASP] perfectly . !sent! Positive',
-    'And I may be the only one but I am really liking [ASP]Windows 8[ASP] . !sent! Positive',
-]
-sent_classifier = APCCheckpointManager.get_sentiment_classifier(checkpoint='fast_lsa_s_acc_84.9_f1_82.11.zip',
+# examples = [
+#     'The [ASP]battery-life[ASP], and this [ASP]battery[ASP] is ok',
+#     'The [ASP] battery-life [ASP] is bad',
+#     'The [ASP] battery-life [ASP] is good',
+#     'The [ASP] battery-life [ASP] ',
+#     'Strong build though which really adds to its [ASP]durability[ASP] .',  # !sent! Positive
+#     'Strong [ASP]build[ASP] though which really adds to its durability . !sent! Positive',
+#     'The [ASP]battery life[ASP] is excellent - 6-7 hours without charging . !sent! Positive',
+#     'I have had my computer for 2 weeks already and it [ASP]works[ASP] perfectly . !sent! Positive',
+#     'And I may be the only one but I am really liking [ASP]Windows 8[ASP] . !sent! Positive',
+# ]
+sent_classifier = APCCheckpointManager.get_sentiment_classifier(checkpoint='fast_lsa_s_acc_85',
                                                                 auto_device=True,  # Use CUDA if available
                                                                 )
 
 # text = 'everything is always cooked to perfection , the [ASP]service[ASP] is excellent , the [ASP]decor[ASP] cool and understated . !sent! 1 1'
 # sent_classifier.infer(text, print_result=True)
 
-inference_sets = examples
+# inference_sets = examples
+#
+# for ex in examples:
+#     result = sent_classifier.infer(ex, print_result=True)
 
-for ex in examples:
-    result = sent_classifier.infer(ex, print_result=True)
-
-inference_sets = ABSADatasetList.English
+inference_sets = ABSADatasetList.Laptop14
 results = sent_classifier.batch_infer(target_file=inference_sets,
                                       print_result=True,
                                       save_result=True,
                                       ignore_error=False,
                                       )
-print(results)
+# print(results)
--- pyabsa/__init__.py
+++ pyabsa/__init__.py
@@ -6,7 +6,7 @@
 
 # Copyright (C) 2021. All Rights Reserved.
 
-__version__ = '1.6.13a0'
+__version__ = '1.6.14'
 __name__ = 'pyabsa'
 
 from termcolor import colored
