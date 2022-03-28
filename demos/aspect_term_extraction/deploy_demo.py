713f1ed1786d6f480eec5068d5aa4f56782f878a
yangheng95 <yangheng@m.scnu.edu.cn>
2022-03-28 21:28:51 +0100

    1.9.1

--- demos/aspect_term_extraction/deploy_demo.py
+++ demos/aspect_term_extraction/deploy_demo.py
@@ -10,40 +10,41 @@ import pandas as pd
 
 from pyabsa import ATEPCCheckpointManager
 
-aspect_extractor = ATEPCCheckpointManager.get_aspect_extractor(checkpoint='fast_lcf_atepc_Chinese_cdw_apcacc_96.69_apcf1_96.25_atef1_92.26',
-                                                               auto_device=True  # False means load model on CPU
-                                                               )
 
+class AspectExtractor:
+    def __init__(self, checkpoint_name='english'):
+        self.inference_model = ATEPCCheckpointManager.get_aspect_extractor(checkpoint=checkpoint_name,
+                                                                           auto_device=True  # False means load model on CPU
+                                                                           )
 
-def inference(text):
-    result = aspect_extractor.extract_aspect(inference_source=[text],
-                                             save_result=False,
-                                             print_result=False,
-                                             pred_sentiment=True)
+    def inference(self, text):
+        result = self.inference_model.extract_aspect(inference_source=[text],
+                                                     pred_sentiment=True)
 
-    result = pd.DataFrame({
-        'aspect': result[0]['aspect'],
-        'sentiment': result[0]['sentiment']
-    })
+        result = pd.DataFrame({
+            'aspect': result[0]['aspect'],
+            'sentiment': result[0]['sentiment']
+        })
 
-    return result
+        return result
 
 
 if __name__ == '__main__':
+    aspect_extractor = AspectExtractor('english')
     iface = gr.Interface(
-        fn=inference,
+        fn=aspect_extractor.inference,
         inputs=["text"],
-        examples=[['the wine list is incredible and extensive and diverse , the food is all incredible and the staff was all very nice , good at their jobs and cultured .'],
-                  ['中 药 学 这 门 课 程 总 体 设 计 非 常 好 简 洁 实 用 重 在 功 效 主 治 病 症 用 法 但 涉 及 的 药 物 数 量 少 只 介 绍 了 不 同 类 中 有 代 表 性 典 型 的 数 味 药 有 些 常 用 药 还 得 课 外 去 进 一 步 了 解'],
-                  [
-                      'Though the menu includes some unorthodox offerings (a peanut butter roll, for instance), the classics are pure and great--we have never had better sushi anywhere, including Japan.'],
-                  ['Everything , from the soft bread , soggy salad , and 50 minute wait time , with an incredibly rude service to deliver below average food .'],
-                  [
-                      'Even though it is running Snow Leopard , 2.4 GHz C2D is a bit of an antiquated CPU and thus the occasional spinning wheel would appear when running Office Mac applications such as Word or Excel .'],
-                  ],
+        examples=[
+            ['the wine list is incredible and extensive and diverse , the food is all incredible and the staff was all very nice , good at their jobs and cultured .'],
+            ['Though the menu includes some unorthodox offerings (a peanut butter roll, for instance), the classics are pure and great--we have never had better sushi anywhere, including Japan.'],
+            ['Everything , from the soft bread , soggy salad , and 50 minute wait time , with an incredibly rude service to deliver below average food .'],
+            ['Even though it is running Snow Leopard , 2.4 GHz C2D is a bit of an antiquated CPU and thus the occasional spinning wheel would appear when running Office Mac applications such as Word or Excel .'],
+            # ['中药学这门课程总体设计非常好简洁实用重在功效主治病症用法但涉及的药物数量少只介绍了不同类中有代表性典型的数味药有些常用药还得课外去进一步了解'],
+        ],
+        live=True,
         outputs="dataframe",
-        theme='huggingface',
-        title='Aspect Term Extraction & Polarity Classification'
+        allow_flagging='auto',
+        title='Aspect Term Extraction & Polarity Classification (English)'
     )
 
-    iface.launch(share=True, )
+    iface.launch(share=True)
