1bb717cd2ce2251e548be6dd7857c09ffb2fc9fb
yangheng95 <yangheng@m.scnu.edu.cn>
2022-04-17 11:01:17 +0100

    1.10.3

--- pyabsa/core/apc/models/fast_lsa_s_v2.py
+++ pyabsa/core/apc/models/fast_lsa_s_v2.py
@@ -58,9 +58,12 @@ class FAST_LSA_S_V2(nn.Module):
         # # --------------------------------------------------- #
 
         if 'lr' == self.opt.window or 'rl' == self.opt.window:
-            if self.opt.eta != 0:
-                cat_features = torch.cat(
-                    (lcf_features, self.eta1 * left_lcf_features, self.eta2 * right_lcf_features), -1)
+            if self.eta1 <= 0 or self.eta2 <= 0:
+                torch.nn.init.uniform_(self.eta1)
+                torch.nn.init.uniform_(self.eta2)
+                print('reset eta(s) to {} {}'.format(self.eta1.item(), self.eta2.item()))
+            if self.opt.eta >= 0:
+                cat_features = torch.cat((lcf_features, self.eta1 * left_lcf_features, self.eta2 * right_lcf_features), -1)
             else:
                 cat_features = torch.cat((lcf_features, left_lcf_features, right_lcf_features), -1)
             sent_out = self.linear_window_3h(cat_features)
