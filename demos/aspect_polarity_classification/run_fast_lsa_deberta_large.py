751fa056e9e13a0eca86f9f8eae3177c07afbffb
yangheng95 <yangheng@m.scnu.edu.cn>
2022-04-27 17:45:01 +0100

    update_doc

--- demos/aspect_polarity_classification/run_fast_lsa_deberta_large.py
+++ demos/aspect_polarity_classification/run_fast_lsa_deberta_large.py
@@ -84,31 +84,31 @@ device = autocuda.auto_cuda()
 #         auto_device=device  # automatic choose CUDA or CPU
 #         )
 #
-# config = APCConfigManager.get_apc_config_english()
-# config.model = APCModelList.FAST_LSA_S_V2
-# config.lcf = 'cdw'
-# config.similarity_threshold = 1
-# config.max_seq_len = 80
-# config.hidden_dim = 1024
-# config.embed_dim = 1024
-# config.dropout = 0
-# config.cache_dataset = False
-# config.patience = 15
-# config.optimizer = 'adam'
-# # config.pretrained_bert = 'yangheng/deberta-v3-large-absa'
+config = APCConfigManager.get_apc_config_english()
+config.model = APCModelList.FAST_LSA_S_V2
+config.lcf = 'cdw'
+config.similarity_threshold = 1
+config.max_seq_len = 80
+config.hidden_dim = 1024
+config.embed_dim = 1024
+config.dropout = 0
+config.cache_dataset = False
+config.patience = 15
+config.optimizer = 'adam'
+config.pretrained_bert = 'yangheng/deberta-v3-large-absa'
 # config.pretrained_bert = 'microsoft/deberta-v3-large'
-# config.num_epoch = 50
-# config.log_step = 5
-# config.SRD = 3
-# config.lsa = True
-# config.eta = 0.5
-# config.learning_rate = 1e-5
-# config.batch_size = 16
-# config.evaluate_begin = 0
-# config.l2reg = 1e-8
-# config.seed = seeds
-# config.cross_validate_fold = -1  # disable cross_validate
-#
+config.num_epoch = 50
+config.log_step = 5
+config.SRD = 3
+config.lsa = True
+config.eta = 0.5
+config.learning_rate = 1e-5
+config.batch_size = 16
+config.evaluate_begin = 0
+config.l2reg = 1e-8
+config.seed = seeds
+config.cross_validate_fold = -1  # disable cross_validate
+
 # dataset = ABSADatasetList.Laptop14
 # config.MV = MetricVisualizer(config.model.__name__ + '-' + dataset.dataset_name, trial_tag='Model & Dataset',
 #                              trial_tag_list=[config.model.__name__ + '-' + dataset.dataset_name])
@@ -127,16 +127,16 @@ device = autocuda.auto_cuda()
 #         auto_device=device  # automatic choose CUDA or CPU
 #         )
 #
-# config.log_step = -1
-# config.patience = 5
-# dataset = ABSADatasetList.MAMS
-# config.MV = MetricVisualizer(config.model.__name__ + '-' + dataset.dataset_name, trial_tag='Model & Dataset',
-#                              trial_tag_list=[config.model.__name__ + '-' + dataset.dataset_name])
-# Trainer(config=config,
-#         dataset=dataset,  # train set and test set will be automatically detected
-#         checkpoint_save_mode=0,  # =None to avoid save model
-#         auto_device=device  # automatic choose CUDA or CPU
-#         )
+config.log_step = -1
+config.patience = 5
+dataset = ABSADatasetList.MAMS
+config.MV = MetricVisualizer(config.model.__name__ + '-' + dataset.dataset_name, trial_tag='Model & Dataset',
+                             trial_tag_list=[config.model.__name__ + '-' + dataset.dataset_name])
+Trainer(config=config,
+        dataset=dataset,  # train set and test set will be automatically detected
+        checkpoint_save_mode=0,  # =None to avoid save model
+        auto_device=device  # automatic choose CUDA or CPU
+        )
 
 config = APCConfigManager.get_apc_config_english()
 config.model = APCModelList.BERT_SPC_V2
@@ -149,8 +149,8 @@ config.dropout = 0
 config.cache_dataset = False
 config.patience = 15
 config.optimizer = 'adam'
-# config.pretrained_bert = 'yangheng/deberta-v3-large-absa'
-config.pretrained_bert = 'microsoft/deberta-v3-large'
+config.pretrained_bert = 'yangheng/deberta-v3-large-absa'
+# config.pretrained_bert = 'microsoft/deberta-v3-large'
 config.num_epoch = 50
 config.log_step = 5
 config.SRD = 3
