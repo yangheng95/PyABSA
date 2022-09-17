718e7b3038478802154eb2d924922147c8997e55
yangheng95 <yangheng@m.scnu.edu.cn>
2022-09-17 01:55:51 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
@@ -46,7 +46,7 @@ extensions = [
     'autoapi.extension'
 ]
 autoapi_type = 'python'
-autoapi_dirs = ['../../pyabsa']
+autoapi_dirs = ['../pyabsa']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = []
