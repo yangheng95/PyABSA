718fed50aa376a4a546823a7af9e4e81804d24c5
yangheng95 <yangheng@m.scnu.edu.cn>
2022-11-12 17:32:07 +0000

    update

--- examples-v2/aspect_polarity_classification/train_apc.py
+++ examples-v2/aspect_polarity_classification/train_apc.py
@@ -17,33 +17,34 @@ import warnings
 warnings.filterwarnings('ignore')
 
 for dataset in [
-    # APCDatasetList.Laptop14,
+    APCDatasetList.Laptop14,
     APCDatasetList.Restaurant14,
     APCDatasetList.Restaurant15,
     APCDatasetList.Restaurant16,
+    # APCDatasetList.MAMS
 ]:
-    config = APC.APCConfigManager.get_apc_config_english()
-    config.model = APC.APCModelList.FAST_LSA_T_V2
-    # config.model = APC.APCModelList.FAST_LSA_S_V2
-    # config.model = APC.APCModelList.BERT_SPC_V2
-    config.pretrained_bert = 'microsoft/deberta-v3-base'
-    config.evaluate_begin = 5
-    config.max_seq_len = 80
-    config.num_epoch = 30
-    config.log_step = 5
-    config.dropout = 0
-    config.cache_dataset = False
-    config.l2reg = 1e-8
-    config.lsa = True
-    config.seed = [random.randint(0, 10000) for _ in range(5)]
-
-    sent_classifier = APC.APCTrainer(config=config,
-                                     dataset=dataset,
-                                     checkpoint_save_mode=ModelSaveOption.SAVE_MODEL_STATE_DICT,
-                                     auto_device=DeviceTypeOption.AUTO,
-                                     # load_aug=True
-                                     ).load_trained_model()
-
+    # config = APC.APCConfigManager.get_apc_config_english()
+    # config.model = APC.APCModelList.FAST_LSA_T_V2
+    # # config.model = APC.APCModelList.FAST_LSA_S_V2
+    # # config.model = APC.APCModelList.BERT_SPC_V2
+    # config.pretrained_bert = 'microsoft/deberta-v3-base'
+    # config.evaluate_begin = 5
+    # config.max_seq_len = 80
+    # config.num_epoch = 30
+    # config.log_step = -1
+    # config.dropout = 0
+    # config.cache_dataset = False
+    # config.l2reg = 1e-8
+    # config.lsa = True
+    # config.seed = [random.randint(0, 10000) for _ in range(5)]
+    #
+    # APC.APCTrainer(config=config,
+    #                dataset=dataset,
+    #                checkpoint_save_mode=ModelSaveOption.SAVE_MODEL_STATE_DICT,
+    #                auto_device=DeviceTypeOption.AUTO,
+    #                # load_aug=True
+    #                ).destroy()
+    #
     # config = APC.APCConfigManager.get_apc_config_english()
     # # config.model = APC.APCModelList.FAST_LSA_T_V2
     # config.model = APC.APCModelList.FAST_LSA_S_V2
@@ -52,41 +53,41 @@ for dataset in [
     # config.evaluate_begin = 5
     # config.max_seq_len = 80
     # config.num_epoch = 30
-    # config.log_step = 5
+    # config.log_step = -1
     # config.dropout = 0
     # config.cache_dataset = False
     # config.l2reg = 1e-8
     # config.lsa = True
     # config.seed = [random.randint(0, 10000) for _ in range(5)]
     #
-    # sent_classifier = APC.APCTrainer(config=config,
-    #                                  dataset=dataset,
-    #                                  checkpoint_save_mode=ModelSaveOption.SAVE_MODEL_STATE_DICT,
-    #                                  auto_device=DeviceTypeOption.AUTO,
-    #                                  # load_aug=True
-    #                                  ).load_trained_model()
-    #
-    # config = APC.APCConfigManager.get_apc_config_english()
-    # # config.model = APC.APCModelList.FAST_LSA_T_V2
-    # # config.model = APC.APCModelList.FAST_LSA_S_V2
-    # config.model = APC.APCModelList.BERT_SPC_V2
-    # config.pretrained_bert = 'microsoft/deberta-v3-base'
-    # config.evaluate_begin = 5
-    # config.max_seq_len = 80
-    # config.num_epoch = 30
-    # config.log_step = 5
-    # config.dropout = 0
-    # config.cache_dataset = False
-    # config.l2reg = 1e-8
-    # config.lsa = True
-    # config.seed = [random.randint(0, 10000) for _ in range(5)]
+    # APC.APCTrainer(config=config,
+    #                dataset=dataset,
+    #                checkpoint_save_mode=ModelSaveOption.SAVE_MODEL_STATE_DICT,
+    #                auto_device=DeviceTypeOption.AUTO,
+    #                # load_aug=True
+    #                ).destroy()
 
-    # sent_classifier = APC.APCTrainer(config=config,
-    #                                  dataset=dataset,
-    #                                  checkpoint_save_mode=ModelSaveOption.SAVE_MODEL_STATE_DICT,
-    #                                  auto_device=DeviceTypeOption.AUTO,
-    #                                  # load_aug=True
-    #                                  ).load_trained_model()
+    config = APC.APCConfigManager.get_apc_config_english()
+    # config.model = APC.APCModelList.FAST_LSA_T_V2
+    # config.model = APC.APCModelList.FAST_LSA_S_V2
+    config.model = APC.APCModelList.BERT_SPC_V2
+    config.pretrained_bert = 'microsoft/deberta-v3-base'
+    config.evaluate_begin = 5
+    config.max_seq_len = 80
+    config.num_epoch = 30
+    config.log_step = -1
+    config.dropout = 0
+    config.cache_dataset = False
+    config.l2reg = 1e-8
+    config.lsa = True
+    config.seed = [random.randint(0, 10000) for _ in range(5)]
+
+    APC.APCTrainer(config=config,
+                   dataset=dataset,
+                   checkpoint_save_mode=ModelSaveOption.SAVE_MODEL_STATE_DICT,
+                   auto_device=DeviceTypeOption.AUTO,
+                   # load_aug=True
+                   ).destroy()
 
     # text = 'everything is always cooked to perfection , the [ASP]service[ASP] is excellent , the [ASP]decor[ASP] cool and understated . $LABEL$ Positive, Positive'
     # sent_classifier.predict(text, print_result=True)
