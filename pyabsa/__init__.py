d43fc6ba38801d6d4dc852aa3be5f54b03a1b37f
yangheng95 <yangheng@m.scnu.edu.cn>
2023-01-28 11:45:37 +0000

    minor_update

--- pyabsa/__init__.py
+++ pyabsa/__init__.py
@@ -7,7 +7,7 @@
 # Copyright (C) 2021. All Rights Reserved.
 
 __name__ = "pyabsa"
-__version__ = "2.0.25.2"
+__version__ = "2.0.26"
 
 from pyabsa.framework.flag_class import *
 
--- unit_test/test_8_augmentation.py
+++ unit_test/test_8_augmentation.py
@@ -22,6 +22,74 @@ import shutil
 
 warnings.filterwarnings("ignore")
 
+# #
+# def test_classification_augmentation():
+#
+#     if os.path.exists("integrated_datasets"):
+#         shutil.rmtree("integrated_datasets")
+#     download_all_available_datasets()
+#
+#     config = TC.TCConfigManager.get_tc_config_english()
+#     config.model = TC.BERTTCModelList.BERT_MLP
+#     # config.pretrained_bert = "bert-base-uncased"
+#     config.num_epoch = 5
+#     config.evaluate_begin = 0
+#     config.max_seq_len = 3
+#     config.dropout = 0.5
+#     config.seed = {42}
+#     config.log_step = -1
+#     config.l2reg = 0.00001
+#
+#     auto_classification_augmentation(
+#         config=config,
+#         dataset=TC.TCDatasetList.SST2,
+#         device=autocuda.auto_cuda(),
+#         # boosting_fold=2,
+#         # classifier_training_num=1,
+#         # augment_num_per_case=2,
+#         # winner_num_per_case=1,
+#         boosting_fold=4,
+#         classifier_training_num=1,
+#         augment_num_per_case=10,
+#         winner_num_per_case=5,
+#         augment_backend="ContextualWordEmbsAug",
+#     )
+#
+#
+# def test_aspect_sentiment_classification_augmentation():
+#
+#     if os.path.exists("integrated_datasets"):
+#         shutil.rmtree("integrated_datasets")
+#     download_all_available_datasets()
+#
+#     config = APC.APCConfigManager.get_apc_config_english()
+#     # config.pretrained_bert = "bert-base-uncased"
+#     config.evaluate_begin = 0
+#     config.max_seq_len = 10
+#     config.num_epoch = 10
+#     config.log_step = 10
+#     config.dropout = 0
+#     config.cache_dataset = False
+#     config.l2reg = 1e-8
+#     config.lsa = True
+#
+#     config.seed = [random.randint(0, 10000) for _ in range(1)]
+#
+#     auto_aspect_sentiment_classification_augmentation(
+#         config=config,
+#         dataset=APC.APCDatasetList.Restaurant16,
+#         device=autocuda.auto_cuda(),
+#         # boosting_fold=2,
+#         # classifier_training_num=1,
+#         # augment_num_per_case=2,
+#         # winner_num_per_case=1,
+#         boosting_fold=4,
+#         classifier_training_num=1,
+#         augment_num_per_case=10,
+#         winner_num_per_case=5,
+#         augment_backend="ContextualWordEmbsAug",
+#     )
+
 
 def test_classification_augmentation():
 
@@ -39,7 +107,7 @@ def test_classification_augmentation():
     config.seed = {42}
     config.log_step = -1
     config.l2reg = 0.00001
-    # config.data_num = 20
+    config.data_num = 20
 
     auto_classification_augmentation(
         config=config,
@@ -81,14 +149,14 @@ def test_aspect_sentiment_classification_augmentation():
         config=config,
         dataset=APC.APCDatasetList.Restaurant16,
         device=autocuda.auto_cuda(),
-        # boosting_fold=2,
-        # classifier_training_num=1,
-        # augment_num_per_case=2,
-        # winner_num_per_case=1,
-        boosting_fold=4,
+        boosting_fold=2,
         classifier_training_num=1,
-        augment_num_per_case=10,
-        winner_num_per_case=5,
+        augment_num_per_case=2,
+        winner_num_per_case=1,
+        # boosting_fold=4,
+        # classifier_training_num=1,
+        # augment_num_per_case=10,
+        # winner_num_per_case=5,
         augment_backend="SynonymAug",
     )
 
